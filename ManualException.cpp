5527359d5cad9d2c6fec855c4af216ecb94b5e92
BagasGandhi <saputramuhammadgandhi@gmail.com>
2026-06-02 16:36:41 +0700

    memperbaiki struktur try-catch untuk pengujian error

--- ManualException.cpp
+++ ManualException.cpp
@@ -6,11 +6,19 @@ int main()
     //membuat isi dari try untuk pengujian error
     try
     {
-
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integrer maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
     }
-
+    //kondisi jika throw yang bertipe data interger
     catch (int a)
     {
-
+        cout << "Pengucualian akan di eksekusi" << endl;
     }
+    //kondisi jika throw bertipe data selain interger
+    catch (...)
+    {
+        cout << "default pengucualian dieksekusi" << endl;
+    }
+    return 0;
 }
